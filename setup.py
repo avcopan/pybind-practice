11993986ce96d0d6acdf22f0e870567deb5a9260
avcopan <avcopan@gmail.com>
2018-06-08 11:18:56 -0500

    ....

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ module1 = Extension('example',
                     sources=['example.cpp'],
                     include_dirs=[pybind11.get_include(False),
                                   pybind11.get_include(False)],
+                    extra_compile_args=['-O3', '-Wall', '-shared', '-std=c++11',
+                                        '-fPIC'],
                     language='c++')
 
 setup(name='example',
